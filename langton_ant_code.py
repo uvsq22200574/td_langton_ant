1d86be98365e063bd84b775c0b4803f5c82e9047
uvsq22200574 <113438074+uvsq22200574@users.noreply.github.com>
2023-02-20 20:52:59 +0100

    Update langton_ant_code.py

--- langton_ant_code.py
+++ langton_ant_code.py
@@ -1,7 +1,30 @@
 from time import time
 from colorama import init, Fore, Style
 from datetime import datetime
+
 init()
+
+
+def render(table):
+    for index in range(len(table)):
+        print(*table[index], end='\n')
+
+
+def start(height, width):
+    return ([[-1 for _ in range(width)] for _ in range(height)])
+
+
+def start_ant(height, width):
+    return ([[[0, "N"] for _ in range(width)] for _ in range(height)])
+
+
+def topile(pile, pil):
+    return (pile + [pil])
+
+
+def unpile(pile):
+    return (pile[:-1], pile[-1])
+
 
 if __name__ == '__main__':
     pass
