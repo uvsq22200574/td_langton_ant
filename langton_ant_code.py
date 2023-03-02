4449dcd93cb8690d7bf2bcd6e7cf0c75d63f3392
uvsq22200574 <113438074+uvsq22200574@users.noreply.github.com>
2023-03-02 19:08:55 +0100

    Added week's progress
    Added all useful functions and corrections. Can now simulate Langton's Ant. Performance issues with multiple elements. Errors needs to be ignored. Missing some functions, like cell editing or backwards simulation...

--- langton_ant_code.py
+++ langton_ant_code.py
@@ -1,22 +1,113 @@
+from colorama import init, Fore, Style
+from datetime import datetime
+
+init()
+
+angles = ["E", "N", "O", "S"]
+
+
+def time_log(simple_format=0):  # Will estimate the precise time at which it has been executed  # noqa: E501
+    if simple_format == 1:
+        return ('%s' % (datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+    return ('[%s]: ' % (datetime.now().strftime("%d/%m/%Y | %H:%M:%S")))
+
+
+def progress_bar(progress, total, init_msg='', end_msg='', mode=0):
+    percent, current_time = (progress * 100) / float(total), time_log(1)
+    if percent % 2 == 0:
+        bar = '▓' * int((percent)/2) + '░' * (int((100 - percent)/2))
+    else:
+        bar = '▓' * int((percent)/2) + '░' * (int((100 - percent)/2) + 1)
+
+    if mode == 0:
+        if progress/total >= 1:
+            print(Fore.GREEN + '╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + end_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+        elif progress/total >= .5:
+            print(Fore.YELLOW + '╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+        else:
+            print(Fore.RED + '╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+    else:
+        if progress/total >= 1:
+            return ('╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + end_msg + '\r', "#00FF00")  # noqa: E501
+        elif progress/total >= .5:
+            return ('╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + init_msg + '\r', "#FFFF00")  # noqa: E501
+        else:
+            return ('╠%s╣ %.3f %% at %s' % (bar, percent, current_time) + ' ' + init_msg + '\r', "#FF0000")  # noqa: E501
+
+
+def count(table, mode=1):
+    count = 0
+    for y in range(len(table)):
+        for x in range(len(table[0])):
+            if mode:
+                count += (table[y][x][0] if table[y][x][0] > 0 else 0)
+            else:
+                count += (table[y][x] if table[y][x] > 0 else 0)
+    percentage = ((count * 100) / ((len(table) - 2) * (len(table[0]) - 2)))
+    return str(count) + ' / ' + str((len(table) - 2) * (len(table[0]) - 2)) + ' ' + '({:.02f}%)'.format(percentage)  # noqa: E501
+
+
+def start(height, width, value=-1):
+    return ([[-1 for _ in range(width)] for _ in range(height)])
+
+
+def start_ant(height, width, value=[0, "N"]):
+    return ([[[0, "N"] for _ in range(width)] for _ in range(height)])
+
+
 def render(table):
     for index in range(len(table)):
         print(*table[index], end='\n')
 
 
-def start(height, width):
-    return ([[-1 for _ in range(width)] for _ in range(height)])
+def read(table, x, y):
+    return (table[y][x])
 
 
-def start_ant(height, width):
-    return ([[[0, "N"] for _ in range(width)] for _ in range(height)])
+def read_ant(table, x, y, mode=1):
+    if mode:
+        return (table[y][x][0])
+    return (table[y][x][1])
 
 
-def topile(pile, pil):
-    return (pile + [pil])
+def case_selector(direction, value, multiplier=1):
+
+    if value:
+        return multiplier * [[0, 1], [-1, 0], [0, -1], [1, 0]][["E", "N", "O", "S"].index(direction)]  # noqa: E501
+    return multiplier * [[0, -1], [1, 0], [0, 1], [-1, 0]][["E", "N", "O", "S"].index(direction)]  # noqa: E501
 
 
-def unpile(pile):
-    return (pile[:-1], pile[-1])
+def interval(x, length_grid):
+    if 1 <= x < length_grid-1:
+        return x
+    elif x == 0:
+        return length_grid-2
+    else:
+        return (x % (length_grid-1)) + 1
+
+
+def next_gen(previous_table_main, previous_table_ant, width, height):
+    new_table_main = start(len(previous_table_main), len(previous_table_main[0]))  # noqa: E501
+    new_table_ant = start_ant(len(previous_table_ant), len(previous_table_ant[0]))  # noqa: E501
+
+    for x in range(1, len(previous_table_ant[0]) - 1):
+        for y in range(1, len(previous_table_ant) - 1):
+            # [Cell State], [Turn, Cell change, Movement], [Cell color], [Rule Name]  # noqa: E501
+
+            # Keep cells in their state
+            if previous_table_main[y][x] == 1:
+                new_table_main[y][x] = 1
+
+            if previous_table_ant[y][x][0]:     # If there is an ant
+                if previous_table_main[y][x] == -1:     # If the cell is white
+                    directions = case_selector(previous_table_ant[y][x][1], -1, 2)    # Select ant's new orientation  # noqa: E501
+                    new_table_ant[interval(y+directions[0], height)][interval(x+directions[1], width)] = [1, angles[(angles.index(previous_table_ant[y][x][1])-1) % 4]]    # Generate new ant's attributes  # noqa: E501
+                else:
+                    directions = case_selector(previous_table_ant[y][x][1], 1, 2)  # noqa: E501
+                    new_table_ant[interval(y-directions[0], height)][interval(x-directions[1], width)] = [1,  angles[(angles.index(previous_table_ant[y][x][1])+1) % 4]]  # noqa: E501
+                new_table_main[y][x] = -previous_table_main[y][x]   # Switch cell state  # noqa: E501
+
+    return (new_table_main, new_table_ant)
 
 
 if __name__ == '__main__':
--- langton_ant_main.py
+++ langton_ant_main.py
@@ -1,89 +1,25 @@
+from langton_ant_code import start, start_ant, time_log, count, read, read_ant, next_gen, progress_bar
 from tkinter import Tk, Canvas, Label, Menu
 from ctypes import windll
-from time import time
-from colorama import init, Fore, Style
-from datetime import datetime
-from PIL import Image, ImageTk
-
-init()
 
 # Window size
 user32 = windll.user32
 user32.SetProcessDPIAware()
 # Window size END
 
-# Functions
-
-# Utility
-
-
-def func_name():
-    ''' Allows to identify the exact function. Use as a string variable.'''
-    from traceback import extract_stack
-    return (__name__ + '(' + extract_stack(None, 2)[0][2]) + ')'
+# Simulation parameters
+height, width, posx, posy, pause = 100, 100, 10, 10, -1
+# Correction
+height += 2
+width += 2
+# END
 
 
-def time_log(simple_format=0):  # Will estimate the precise time at which it has been executed
-    if simple_format == 1:
-        return ('%s' % (datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
-    return ('[%s]: ' % (datetime.now().strftime("%d/%m/%Y | %H:%M:%S")))
-
-
-def progress_bar(progress, total, init_msg='', end_msg='', mode=0):
-    percent, time = (progress * 100) / float(total), time_log(1)
-    if percent % 2 == 0:
-        bar = '▓' * int((percent)/2) + '░' * (int((100 - percent)/2))
-    else:
-        bar = '▓' * int((percent)/2) + '░' * (int((100 - percent)/2) + 1)
-
-    if mode == 0:
-        if progress/total >= 1:
-            print(Fore.GREEN + '╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + end_msg + Style.RESET_ALL, sep=' ', end='\n')
-        elif progress/total >= .5:
-            print(Fore.YELLOW + '╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')
-        else:
-            print(Fore.RED + '╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')
-    else:
-        if progress/total >= 1:
-            return ('╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + end_msg + '\r', "#00FF00")
-        elif progress/total >= .5:
-            return ('╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + init_msg + '\r', "#FFFF00")
-        else:
-            return ('╠%s╣ %.3f %% at %s' % (bar, percent, time) + ' ' + init_msg + '\r', "#FF0000")
-
-# Utility END
-def start(height: int, width: int):
-    '''Will generate the base grid that contains the cells.'''
-    return ([[-1 for _ in range(width)] for _ in range(height)])
-
-
-def start_ant(height: int, width: int):
-    '''Will generate the base grid that contains the ants.'''
-    return ([[[0, "N"] for _ in range(width)] for _ in range(height)])
-
-
-def read(table: list, x: int, y: int):
-    ''' Returns the value based on coordinates. Made for the cell's grid.'''
-    return (table[y][x])
-
-
-def read_ant(table: list, x: int, y: int, mode=1):
-    '''
-    Returns the value based on coordinates. Made for the ant's grid.\n
-    Possibility to return either the existence of an ant or the orientation.\n
-    The default ant value is 0 (No Ant) and orientation is "N" (North).\n
-    Orientation is independant of an Ant.
-    '''
-    if mode:
-        return (table[y][x][0])
-    return (table[y][x][1])
-
-
-def rectangle(grid: list, x: int, y: int, x_length: int, y_length: int, value=1):
-    '''
-    Used to create a rectangle based on coordinates.\n
-    Used by the place_action function, that triggers when there's a click.
-    '''
+def rectangle(grid, x, y, x_length, y_length, value=1):
+    """
+    Used to create a rectangle based on coordinates.
+    Used by the place_action function, that triggers when there's a click Button-x
+    """
 
     if x in [0, len(grid[0])]:
         x = 1
@@ -96,60 +32,217 @@ def rectangle(grid: list, x: int, y: int, x_length: int, y_length: int, value=1)
     return (grid)
 
 
+main_grid, ant_grid, generation, number_of_generations = start(height, width), start_ant(height, width), 0, 20000
+# Main grid, current generation, maximum number of generation
+action, cursor_size_width, cursor_size_height = (rectangle), 1, 1
+
+
+def set_action(new_action=rectangle):
+    """
+    Allow to change the function defined to the variable "action"
+    """
+
+    global action
+    action = new_action
+
+
+def set_grid():
+    global main_grid, history
+    window.after(func=after_loop, ms=0)
+
+
+def stop_sim():
+    """
+    Set the simulation parameters so that it stops itself
+    """
+
+    global generation, number_of_generations
+    generation = number_of_generations
+
+
 def clear_grid():
-    '''Clear both grids and reset to its base state.'''
+    """
+    Clear both grids and reset to its base state
+    """
 
     global main_grid, ant_grid
     main_grid = start(height, width)
     ant_grid = start_ant(height, width)
-    print("From %s: Missing Draw Simulation" % func_name())
+    canevas.delete('all')
+    update_table()
 
-# END
+# Simulation parameters END
 
-# Simulation parameters/variables
-
-
-height, width, posx, posy, pause = 101, 101, 10, 10, -1
-
-main_grid, ant_grid = start(height, width), start_ant(height, width)
-
-action, cursor_size_width, cursor_size_height = (rectangle), 1, 1
-
-generation, number_of_generations, steps_per_gen = 0, 20000, 25
-
-cell_size = 8
-
-screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-
-# [Ant Colors], [Rule Name]
-rules = [["#00ff00", "#ff00ff"], "LANGTON ANT"]
-
-dimensions = ((width * (cell_size)), (height * (cell_size)))
-# Correction
-
-height += 2
-width += 2
-
-# END
-
-# Window definition
+# Window def
 
 
 window = Tk()
-window.title("Langton's Ant")
+window.title("Game of Life")
 window.configure(bg="#000000")
-window.geometry('%dx%d' % (screen_width - 50, screen_height - 50))
 window.state('zoomed')
 
+screen_width = user32.GetSystemMetrics(0)
+screen_height = user32.GetSystemMetrics(1)
+
+window.geometry('%dx%d' % (screen_width - 50, screen_height - 50))
 # Window def END
 
+# Menu def
 
-canevas = Canvas(window, bg="white", height=((height + 2) * (cell_size)), width=((width + 2) * (cell_size)))
+window_menu = Menu(window)
+
+file = Menu(window_menu, tearoff=0)
+window_menu.add_cascade(label='File', menu=file)
+file.add_command(label='Undo', command=set_grid)
+file.add_separator()
+file.add_command(label='Clear', command=clear_grid)
+file.add_command(label='Exit', command=stop_sim)
+
+window.config(menu=window_menu)
+# Menu def END#
+
+# [Cell State], [Turn, Cell change, Movement], [Cell color], [Rule Name]
+# [Movement Behaviour: str], [Change Rule: 0,1], [Ant Color], [Rule Name]
+rules = [
+    ["RL", [-1,1],"#00ff00", "LANGTON ANT"]
+    ]
+current_rules = rules[0]
+
+
+# Size determination #
+
+
+def function(x):
+    return (.9 * 2.718281**(-x/(2.5*2.718281)))
+
+
+def size():
+    screen_width = user32.GetSystemMetrics(0)
+    screen_height = user32.GetSystemMetrics(1)
+    cell_size = min(int((screen_height) / (height) * function(height/100)), int((screen_width) / (width) * function(height/100)))
+    font_size = int((cell_size*width/17) * function((cell_size*width)/10))
+    if cell_size < 2:
+        cell_size = 2
+    return (cell_size, font_size)
+
+
+# Size determination END #
+cell = size()[0]  # Must be an integer
+
+
+dimensions = ((width * (cell)), (height * (cell)))
+
+
+def set_rules(id):
+    global rules, current_rules
+    current_rules = rules[id]
+
+
+canevas = Canvas(window, bg="white", height=((height + 2) * (cell)), width=((width + 2) * (cell)))
 canevas.grid(row=0, column=0)
 
+text1 = Label(text="Test", background="#000000", fg='magenta', font=("Times New Roman", size()[1]))
+text2 = Label(text='Cursor dimensions:' + str(cursor_size_width) + 'x' + str(cursor_size_height) + ' | ' + str(action.__name__) +'    ', background="#000000", fg='#0088ff', font=("Times New Roman", size()[1]))
+text3 = Label(text=progress_bar(generation, number_of_generations, mode=1)[0] + " " + str(generation) + "/" + str(number_of_generations), background="#000000", fg=progress_bar(generation, number_of_generations, mode=1)[1], font=("Times New Roman", size()[1]))
+text1.grid(row=1, column=0)
+text2.grid(row=2, column=0)
+text3.grid(row=3, column=0)
 
-if __name__ == "__main__":
-    pass
 
+def update_table(debug=0):
+    """
+    Draw all squares depending of the state of a cell
+    """
+
+    for h in range(height):
+        for w in range(width):
+            posx, posy = ((w + 1) * cell), ((h + 1) * cell)
+            if h in [0, len(main_grid)-1] or w in [0, len(main_grid[0])-1]:
+                main_grid[h][w] = -1
+                canevas.create_rectangle(posx, posy, posx + cell, posy + cell, fill='#cc0000', outline='#1c1c1c')
+
+            if read(main_grid, w, h) == 1:
+                canevas.create_rectangle(posx, posy, posx + cell, posy + cell, fill="#000000")
+
+            if read_ant(ant_grid, w, h):
+                canevas.create_rectangle(posx, posy, posx + cell, posy + cell, fill=current_rules[2])
+
+
+def after_loop():
+    global main_grid, ant_grid, generation, number_of_generations, history
+    update_table()
+    text1.config(text='Generation n°%d | Rules "%s" | %sx%s | %s | Cell count: %s' % (generation, current_rules[3], dimensions[0], dimensions[1], time_log(1), count(ant_grid)))
+    text2.config(text='Cursor dimensions:' + str(cursor_size_width) + 'x' + str(cursor_size_height) + ' | ' + str(action.__name__))
+    text3.config(text=progress_bar(generation, number_of_generations, mode=1)[0] + " " + str(generation) + "/" + str(number_of_generations), fg=progress_bar(generation, number_of_generations, mode=1)[1])
+    window.update()
+    if pause == -1:
+        canevas.delete('all')
+        update_table()
+        window.after(func=after_loop, ms=0)
+    elif generation < number_of_generations:
+        canevas.delete('all')
+        main_grid, ant_grid = next_gen(main_grid, ant_grid, width, height)
+        generation += 1
+        window.after(func=after_loop, ms=0)
+    else:
+        window.destroy()
+
+
+def place_action(eventorigin):
+
+    global history
+    reg_x = (eventorigin.x) // cell
+    reg_y = (eventorigin.y) // cell
+    action(ant_grid, reg_x - 1, reg_y - 1, cursor_size_width, cursor_size_height, [1, "N"])
+
+
+def destroy_cell(eventorigin):
+
+    global history
+    reg_x = (eventorigin.x) // cell
+    reg_y = (eventorigin.y) // cell
+    action(ant_grid, reg_x - 1, reg_y - 1, cursor_size_width, cursor_size_height, [0, "N"])
+
+
+def key_press(event):
+    global pause, main_grid, ant_grid, generation, current_rules, cursor_size_height, cursor_size_width
+    press = event.keysym
+    if press == 'space':
+        pause = -(pause)
+    elif press == 'f':
+        if pause == 1:
+            pause = -(pause)
+            main_grid, ant_grid = next_gen(main_grid, ant_grid, width, height)
+            generation += 1
+        else:
+            main_grid, ant_grid = next_gen(main_grid, ant_grid, width, height)
+            generation += 1
+    elif press == 'Tab':
+        print('Window forcefully closed by pressing TAB at generation n° %d at %s  ' % (generation, time_log(1)))
+        stop_sim()
+        pause = 1
+    elif press == 'Up':
+        cursor_size_height += 1
+    elif press == 'Down':
+        if cursor_size_height > 1:
+            cursor_size_height -= 1
+    elif press == 'Right':
+        cursor_size_width += 1
+    elif press == 'Left':
+        if cursor_size_width > 1:
+            cursor_size_width -= 1
+
+
+
+# START
+
+window.bind('<Key>', key_press)
+window.bind("<B1-Motion>", place_action)
+window.bind("<Button-1>", place_action)
+window.bind("<B3-Motion>", destroy_cell)
+window.bind("<Button-3>", destroy_cell)
+
+update_table()
+canevas.after(func=after_loop, ms=0)
 
 window.mainloop()
