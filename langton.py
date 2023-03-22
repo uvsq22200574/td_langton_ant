401a104c58665c21771058c848600742febecfa5
uvsq22200574 <113438074+uvsq22200574@users.noreply.github.com>
2023-03-22 23:12:53 +0100

    Out of Bounds Fixes + Files Fusion

--- langton.py
+++ langton.py
@@ -1,8 +1,8 @@
-from langton_code_legacy import start, start_ant, time_log, count, next_gen, progress_bar  # noqa: E501
 from tkinter import Tk, Label, Menu, Checkbutton, IntVar, Button
 from PIL import Image, ImageTk
 from colorama import init, Fore, Style
 from platform import system
+init()
 
 
 """
@@ -34,6 +34,134 @@ else:
 # /=> System Compatibility END <=/
 
 
+# /=> Dependancies <=/
+
+
+def time_log(simple_format=False, tmp=False, tmp2=False):  # noqa: E501
+    from datetime import datetime
+    '''Will estimate the precise time at which it has been executed.'''
+    if tmp2 is True:
+        if simple_format is True:
+            return ('%s' % (datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+        return ('[%s]: ' % (datetime.now().strftime("%d/%m/%Y | %H:%M:%S")))
+    if tmp is True:
+        if simple_format is True:
+            return ('%s' % (datetime.now().strftime("%H:%M:%S")))
+        return ('[%s]: ' % (datetime.now().strftime("%H:%M:%S")))
+    else:
+        if simple_format is True:
+            return ('%s' % (datetime.now().strftime("%d/%m/%Y")))
+        return ('[%s]: ' % (datetime.now().strftime("%d/%m/%Y")))
+
+
+def progress_bar(progress: int, total: int, init_msg='', end_msg='', tmp=0, tmp2=2):  # noqa: E501
+    '''Returns or print a progress bar.'''
+    percent = (progress * 100) / float(total)
+    if percent % 2 == 0:
+        bar = '▓' * int((percent)/tmp2) + '░' * (int((100 - percent)/tmp2))
+    else:
+        bar = '▓' * int((percent)/tmp2) + '░' * (int((100 - percent)/tmp2) + 1)
+
+    if tmp == 0:
+        if progress/total >= 1:
+            print(Fore.GREEN + '╠%s╣ %.3f %%' % (bar, percent) + ' ' + end_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+        elif progress/total >= .5:
+            print(Fore.YELLOW + '╠%s╣ %.3f %%' % (bar, percent) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+        else:
+            print(Fore.RED + '╠%s╣ %.3f %%' % (bar, percent) + ' ' + init_msg + Style.RESET_ALL, sep=' ', end='\r')  # noqa: E501
+    else:
+        if progress/total >= 1:
+            return ('╠%s╣ %.3f %%' % (bar, percent) + ' ' + end_msg + '\r', "#00FF00")  # noqa: E501
+        elif progress/total >= .5:
+            return ('╠%s╣ %.3f %%' % (bar, percent) + ' ' + init_msg + '\r', "#FFFF00")  # noqa: E501
+        else:
+            return ('╠%s╣ %.3f %%' % (bar, percent) + ' ' + init_msg + '\r', "#FF0000")  # noqa: E501
+
+
+def count(table: list, tmp=1, tmp2=0):
+    '''Will return the number of cells/ants of a grid.'''
+
+    count = 0
+    for y in range(len(table)):
+        for x in range(len(table[0])):
+            if tmp:
+                count += (table[y][x][0] if table[y][x][0] > 0 else 0)
+            else:
+                count += (table[y][x] if table[y][x] > 0 else 0)
+    percentage = ((count * 100) / ((len(table) - 2) * (len(table[0]) - 2)))
+    if tmp2:
+        return count
+    return str(count) + ' / ' + str((len(table) - 2) * (len(table[0]) - 2)) + ' ' + '({:.02f}%)'.format(percentage)  # noqa: E501
+
+
+def start(height, width, value=-1):
+    '''Base template of a langton's cell's grid.'''
+
+    return ([[-1 for _ in range(width)] for _ in range(height)])
+
+
+def start_ant(height, width, value=[0, "N"]):
+    '''Base template of a langton's ant's grid.'''
+
+    return ([[[0, "N"] for _ in range(width)] for _ in range(height)])
+
+
+def render(table: list):
+    '''Print a grid in a readable way.'''
+
+    for index in range(len(table)):
+        print(*table[index], end='\n')
+
+
+def case_selector(direction: str, cell_state: int, multiplier=1):
+    '''Translate the direction of an ant to a grid logic.'''
+
+    if cell_state:
+        return [[0, multiplier], [-multiplier, 0], [0, -multiplier], [multiplier, 0]][["E", "N", "O", "S"].index(direction)]  # noqa: E501
+    return multiplier * [[0, -multiplier], [multiplier, 0], [0, multiplier], [-multiplier, 0]][["E", "N", "O", "S"].index(direction)]  # noqa: E501
+
+
+def interval(x: int, length_grid: int):
+    '''Enable the grid to be a tor.\n /!\\ Bug: When placed at grid_length-2 * R² destroy cell'''
+    if 1 <= x < length_grid-1:  # If the ant is in the grid
+        return x
+    elif x == 0:    # Teleport the ant to the other side
+        return length_grid-2
+    return (x % (length_grid-1)) + 1
+
+
+def next_gen(previous_table_main: list, previous_table_ant: list, width: int, height: int, multiplier=1, steps=1):  # noqa:501
+    '''
+    Compute all steps, then return the next grid based on the previous one.
+    '''
+
+    new_table_main = start(len(previous_table_main), len(previous_table_main[0]))  # noqa: E501
+    new_table_ant = start_ant(len(previous_table_ant), len(previous_table_ant[0]))  # noqa: E501
+    angles = ["E", "N", "O", "S"]
+
+    for x in range(1, len(previous_table_ant[0]) - 1):
+        for y in range(1, len(previous_table_ant) - 1):
+            # [Cell State], [Turn, Cell change, Movement], [Cell color], [Rule Name]  # noqa: E501
+
+            # Keep drawing cells if state unchanged
+            if previous_table_main[y][x] == 1:
+                new_table_main[y][x] = 1
+
+            if previous_table_ant[y][x][0]:     # If there is an ant
+                if previous_table_main[y][x] == -1:     # If the cell is white
+                    directions = case_selector(previous_table_ant[y][x][1], -1, multiplier)    # Select ant's new orientation  # noqa: E501
+                    new_table_ant[interval(y+directions[0], height)][interval(x+directions[1], width)] = [1, angles[(angles.index(previous_table_ant[y][x][1])-1) % 4]]  # Set new ant's attributes  # noqa: E501
+                else:
+                    directions = case_selector(previous_table_ant[y][x][1], 1, multiplier)  # noqa: E501
+                    new_table_ant[interval(y-directions[0], height)][interval(x-directions[1], width)] = [1,  angles[(angles.index(previous_table_ant[y][x][1])+1) % 4]]  # noqa: E501
+                new_table_main[y][x] = -previous_table_main[y][x]   # Switch cell state  # noqa: E501
+
+    return (new_table_main, new_table_ant)
+
+
+# /=> Dependancies END <=/
+
+
 def Rectangle(grid, x, y, x_length, y_length, value=1):
     """
     Used to create a rectangle based on coordinates.
@@ -41,24 +169,25 @@ def Rectangle(grid, x, y, x_length, y_length, value=1):
     """
 
     limit = (width-2)*(height-2)
+    print(x+x_length+1, y+y_length+1)
     if x_length*y_length >= limit:
         Sim_feedback.config(text="Cannot place a full grid.", fg="#FF0000")
         return (0)
-    elif (x+x_length+1 > len(grid[0])) and (y+y_length+1 > len(grid)) or (x == 0 and y == 0):  # noqa: E501
+    elif (x+x_length+1 > len(grid[0])) and (y+y_length+1 > len(grid)) or (x <= 0 and y <= 0):  # noqa: E501
         Sim_feedback.config(text="Cannot place out of bounds in x and y axis.", fg="#FF0000")  # noqa: E501
         return (0)
-    elif x+x_length+1 > len(grid[0]) or x == 0:
+    elif (x+x_length+1 > len(grid[0])) or (x <= 0):
         Sim_feedback.config(text="Cannot place out of bounds in x axis.", fg="#FF0000")  # noqa: E501
         return (0)
-    elif y+y_length+1 > len(grid) or y == 0:
+    elif (y+y_length+1 > len(grid)) or (y <= 0):
         Sim_feedback.config(text="Cannot place out of bounds in y axis.", fg="#FF0000")  # noqa: E501
         return (0)
     else:
         Sim_feedback.config(text="No error reported.", fg="#00AA00")
 
-    for xp in range(x_length):
-        for yp in range(y_length):
-            grid[y+yp][x+xp] = value
+        for xp in range(x_length):
+            for yp in range(y_length):
+                grid[y+yp][x+xp] = value
 
 
 # /=> Simulation parameters <=/
@@ -143,38 +272,20 @@ File_selector.add_command(label='Exit', command=stop_sim)
 window.config(menu=window_menu)
 # /=> Window DEF END <=/
 
-# /=> Size Determination <=/
-
-
-def size():
-    '''Used to determine the correct size based on the screen dimensions. WIP.'''  # noqa:501
-
-    def function(x):
-        return (.9 * 2.718281**(-x/(2.5*2.718281)))
-
-    cell_size = min(int((screen_height) / (height) * function(height/100)), int((screen_width) / (width) * function(height/100)))  # noqa: E501
-    font_size = int((cell_size*width/17) * function((cell_size*width)/10))
-    if cell_size < 2:
-        cell_size = 2
-    return (cell_size, font_size)
-
-
-# /=> Size Determination END <=/
-
 
 # /=> Widgets DEF <=/
 sim_graph = Label()
 sim_graph.grid(row=0, column=1, rowspan=height//2, columnspan=3)
 
-Sim_time = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_date = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_Dimensions = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_stats_dimensions = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_feedback = Label(text="Nothing to report.", background="#111111", fg="#AAAAAA", font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_generation = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_cells = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_ant = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", size()[1]))  # noqa: E501
-Sim_progress = Label(text="N/A", background="#111111", font=("Times New Roman", size()[1]))  # noqa: E501
+Sim_time = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", 12))  # noqa: E501
+Sim_date = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", 12))  # noqa: E501
+Sim_Dimensions = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", 12))  # noqa: E501
+Sim_stats_dimensions = Label(text="N/A", background="#111111", fg='#0088ff', font=("Times New Roman", 12))  # noqa: E501
+Sim_feedback = Label(text="Nothing to report.", background="#111111", fg="#AAAAAA", font=("Times New Roman", 12))  # noqa: E501
+Sim_generation = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", 12))  # noqa: E501
+Sim_cells = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", 12))  # noqa: E501
+Sim_ant = Label(text="N/A", background="#111111", fg='magenta', font=("Times New Roman", 12))  # noqa: E501
+Sim_progress = Label(text="N/A", background="#111111", font=("Times New Roman", 12))  # noqa: E501
 
 Sim_time.grid(row=0, column=0)
 Sim_date.grid(row=1, column=0)
@@ -284,18 +395,17 @@ def place_action(eventorigin):
     '''Action of creating a cell.'''
 
     reg_x, reg_y = (eventorigin.x) // cell, (eventorigin.y) // cell
-    if reg_x >= 0 and reg_y >= 0:
-        grid_and_action = [(ant_grid, [1, "N"]), (main_grid, 1)][int(grid_edit.get())]  # noqa: E501
-        action(grid_and_action[0], reg_x, reg_y, cursor_size_width, cursor_size_height, grid_and_action[1])  # noqa: E501
+
+    grid_and_action = [(ant_grid, [1, "N"]), (main_grid, 1)][int(grid_edit.get())]  # noqa: E501
+    action(grid_and_action[0], reg_x, reg_y, cursor_size_width, cursor_size_height, grid_and_action[1])  # noqa: E501
 
 
 def destroy_cell(eventorigin):
     '''Action of destroying a cell.'''
 
     reg_x, reg_y = (eventorigin.x) // cell, (eventorigin.y) // cell
-    if reg_x >= 0 and reg_y >= 0:
-        grid_and_action = [(ant_grid, [0, "N"]), (main_grid, -1)][int(grid_edit.get())]  # noqa: E501
-        action(grid_and_action[0], reg_x, reg_y, cursor_size_width, cursor_size_height, grid_and_action[1])  # noqa: E501
+    grid_and_action = [(ant_grid, [0, "N"]), (main_grid, -1)][int(grid_edit.get())]  # noqa: E501
+    action(grid_and_action[0], reg_x, reg_y, cursor_size_width, cursor_size_height, grid_and_action[1])  # noqa: E501
 
 
 def key_press(event):
